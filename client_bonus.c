4ec14f03fd8e81caadf99da057a9090cb589c892
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-02-05 13:24:39 +0100

    aa

--- client_bonus.c
+++ client_bonus.c
@@ -1,24 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:28:08 by zouazrou          #+#    #+#             */
-/*   Updated: 2025/02/05 11:56:14 by zouazrou         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:56:30 by zouazrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "minitalk_bonus.h"
 
 int		g_ready = 0;
 
 void	ready(int sig)
 {
-	(void)sig;
-	g_ready = 1337;
+	if (sig == SIGUSR1)
+		g_ready = 1337;
+	else if (sig == SIGUSR2)
+		exit((ft_putstr_fd("Message received \n", 1), 0));
 }
+// void	received(void)
 
 void	check_args(int ac, char **av)
 {
@@ -35,14 +38,14 @@ void	check_args(int ac, char **av)
 	}
 }
 
-void	send_t_server(int pid, int dec)
+void	send_t_server(int pid, char c)
 {
 	int	num_of_byte;
 
 	num_of_byte = 8;
 	while (num_of_byte--)
 	{
-		if (!(dec % 2))
+		if (((c >> num_of_byte) % 2) == 0)
 		{
 			if (kill(pid, SIGUSR1) == -1)
 				exit((ft_putstr_fd("error : when send SIGUSR1\n", 1), 1));
@@ -52,7 +55,6 @@ void	send_t_server(int pid, int dec)
 			if (kill(pid, SIGUSR2) == -1)
 				exit((ft_putstr_fd("error : when send SIGUSR2\n", 1), 1));
 		}
-		dec /= 2;
 		while (!g_ready)
 			;
 		g_ready = 0;
@@ -70,11 +72,14 @@ int	main(int ac, char **av)
 	if (kill(pid, 0) == -1)
 		exit((ft_putstr_fd("error : PID invalid\n", 1), 1));
 	signal(SIGUSR1, ready);
+	signal(SIGUSR2, ready);
 	while (*msg)
 	{
 		send_t_server(pid, *msg);
 		msg++;
 	}
 	send_t_server(pid, '\0');
+	while (1949)
+		pause();
 	return (0);
 }
---
+++ utils_bonus.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 09:45:18 by zouazrou          #+#    #+#             */
+/*   Updated: 2025/01/18 11:21:43 by zouazrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+double	ft_pow(double base, int exponent)
+{
+	double	result;
+	int		i;
+
+	i = 0;
+	result = 1.0;
+	if (exponent < 0)
+	{
+		base = 1 / base;
+		exponent = -exponent;
+	}
+	while (i < exponent)
+	{
+		result *= base;
+		i++;
+	}
+	return (result);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n >= 0 && n <= 9)
+	{
+		ft_putchar_fd(n + '0', fd);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+void	is_nb(char *arg)
+{
+	if (!*arg)
+	{
+		ft_putstr_fd("error : PID is empty !\n", 1);
+		exit(1);
+	}
+	while (*arg)
+	{
+		if (*arg < '0' || *arg > '9')
+		{
+			ft_putstr_fd("error : PID is wrong !\n", 1);
+			exit(1);
+		}
+		arg++;
+	}
+}
